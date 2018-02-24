743996be6ac2a53ed8356d8a3980d96e070a7f01
Quentin Rameau <quinq@fifth.space>
2018-02-24 14:39:28 +0100

    Make the screen interface the default

--- config.mk
+++ config.mk
@@ -4,10 +4,10 @@ MANDIR = $(PREFIX)/share/man/man1
 
 # UI type
 # txt (textual)
-UI=txt
+#UI=txt
 # ti (screen-oriented)
-#UI=ti
-#LIBS=-lcurses
+UI=ti
+LIBS=-lcurses
 
 # Stock FLAGS
 SACCCFLAGS = -D_DEFAULT_SOURCE -D_XOPEN_SOURCE=700 -D_BSD_SOURCE $(CFLAGS)
