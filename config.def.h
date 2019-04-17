45f780253a6b3be216395e9cacbf858061354e13
William Casarin <jb55@jb55.com>
2019-04-17 00:19:26 -0700

    jb55's config

--- config.def.h
+++ config.def.h
@@ -5,17 +5,17 @@
 #define _key_entrydown	'J' /* move to next link */
 #define _key_lnup	'k' /* move one line up */
 #define _key_entryup	'K' /* move to previous link */
-#define _key_pgdown	' ' /* move one screen down */
-#define _key_pgup	'b' /* move one screen up */
+#define _key_pgdown	'd' /* move one screen down */
+#define _key_pgup	'u' /* move one screen up */
 #define _key_home	'g' /* move to the top of page */
 #define _key_end	'G' /* move to the bottom of page */
 #define _key_pgnext	'l' /* view highlighted item */
-#define _key_pgprev	'h' /* view previous item */
-#define _key_cururi	'U' /* print page uri */
-#define _key_seluri	'u' /* print item uri */
+#define _key_pgprev	'q' /* view previous item */
+#define _key_cururi	'p' /* print item uri */
+#define _key_seluri	'y' /* print page uri */
 #define _key_fetch	'L' /* refetch current item */
 #define _key_help	'?' /* display help */
-#define _key_quit	'q' /* exit sacc */
+#define _key_quit	'Q' /* exit sacc */
 #define _key_search	'/' /* search */
 #define _key_searchnext	'n' /* search same string forward */
 #define _key_searchprev	'N' /* search same string backward */
@@ -24,7 +24,7 @@
 #define _text_color CYAN BOLD
 
 /* default plumber */
-static char *plumber = "xdg-open";
+static char *plumber = "open";
 
 /* temporary directory template (must end with six 'X' characters) */
 static char tmpdir[] = "/tmp/sacc-XXXXXX";
